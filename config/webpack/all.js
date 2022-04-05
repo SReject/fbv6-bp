993222f9861e94060cc4f2be1ebcdf16b9dcc8d2
SReject <SReject@users.noreply.github.com>
2022-04-05 15:23:02 -0400

    cleanup package scripts

---
+++ config/webpack/all.js
@@ -0,0 +1,5 @@
+const mainConfig = require('./main.js');
+const preloadConfig = require('./preload.js');
+const rendererConfig = require('./renderer.js');
+
+module.exports = [mainConfig, preloadConfig, rendererConfig];
