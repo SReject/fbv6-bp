b1093d805b9bcc6a614a3c897e49d2f32e569ec9
SReject <SReject@users.noreply.github.com>
2022-04-05 14:11:45 -0400

    setup simple app

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,6 +1,37 @@
 // main entry point for backend
-import { app } from 'electron';
+import { app, BrowserWindow } from 'electron';
+
+const isDev = !/^pro(?:d(?:uction)?)?$/.test(process.env.NODE_ENV || '');
+
+let mainWindow: BrowserWindow;
+const createWindow = () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+
+    if (isDev) {
+        mainWindow.loadURL('http://localhost:8080');
+    } else {
+        mainWindow.loadFile('../renderer/index.html');
+    }
+}
+
 
 app.whenReady().then(() => {
     console.log('running');
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+
+    createWindow();
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,2 +1,18 @@
 // renderer main entry point
 import './css/main.css';
+
+import React from "react";
+import { createRoot } from 'react-dom/client';
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <span className="font-bold">Fixed tailwind!!</span>
+        );
+    }
+}
+
+const container = document.getElementById('firebot');
+const root = createRoot(container!);
+root.render(<App />);
