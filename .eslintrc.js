ebd4d29d56d88a1c0e8347c989ee19a222f08019
SReject <SReject@users.noreply.github.com>
2022-04-05 17:21:43 -0400

    ignore non-source directories

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,12 @@ module.exports = {
     env: {
         node: true,
         browser: true
-    }
+    },
+
+    ignorePatterns: [
+        "/node_modules/**/*",
+        "/build/**/*",
+        "/config/**/*",
+        "/dist/**/*"
+    ]
 };
